dc1458d362b37a3da2f3068146b8327e36f9679a
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-04-20 16:58:02 +0500

    make it better

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+var cors = require('cors')
+ 
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -6,7 +9,7 @@ const app = express();
 const path = require('path');
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
-
+app.use(cors())
 
 app.get('/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
